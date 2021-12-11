4064084f90cdc96042c5117e121d64b97d7966e6
Thorsten Lorenz <thlorenz@gmx.de>
2021-12-10 23:24:50 -0700

    types: adding buffer and uint8array

--- src/collections.ts
+++ src/collections.ts
@@ -43,7 +43,7 @@ export function fixedSizeArray<T>(element: Beet<T>, len: number): Beet<T[]> {
     },
     read: function (buf: Buffer, offset: number): T[] {
       const size = u32.read(buf, offset)
-      assert.equal(size, len, `invalid byte size`)
+      assert.equal(size, len, 'invalid byte size')
       const arr: T[] = new Array(len)
       for (let i = 0; i < len; i++) {
         arr[i] = element.read(buf, offset + 4 + i * element.byteSize)
@@ -52,5 +52,38 @@ export function fixedSizeArray<T>(element: Beet<T>, len: number): Beet<T[]> {
     },
     byteSize: 4 + element.byteSize * len,
     description: `collection: Array of ${element.description}`,
+  }
+}
+
+export function fixedSizeBuffer(bytes: number): Beet<Buffer> {
+  return {
+    write: function (buf: Buffer, offset: number, value: Buffer): void {
+      u32.write(buf, offset, bytes)
+      value.copy(buf, offset + 4, 0, bytes)
+    },
+    read: function (buf: Buffer, offset: number): Buffer {
+      const size = u32.read(buf, offset)
+      assert.equal(size, bytes, 'invalid byte size')
+      return buf.slice(offset + 4, offset + 4 + bytes)
+    },
+
+    byteSize: 4 + bytes,
+    description: 'collection: Buffer',
+  }
+}
+
+export function fixedSizeUint8Array(len: number): Beet<Uint8Array> {
+  return {
+    write: function (buf: Buffer, offset: number, value: Uint8Array): void {
+      const valueBuf = Buffer.from(value)
+      fixedSizeBuffer(len).write(buf, offset, valueBuf)
+    },
+    read: function (buf: Buffer, offset: number): Uint8Array {
+      const arrayBuffer = fixedSizeBuffer(len).read(buf, offset)
+      return Uint8Array.from(arrayBuffer)
+    },
+
+    byteSize: 4 + len,
+    description: 'collection: Uint8Array',
   }
 }
