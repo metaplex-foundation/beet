e58624674be2bb3e715ca5a6d281acc4fdc86c77
Thorsten Lorenz <thlorenz@gmx.de>
2022-01-05 14:21:49 -0500

    doc: setup beet-solana docs and adding them

--- beet-solana/src/keys.ts
+++ beet-solana/src/keys.ts
@@ -10,6 +10,11 @@ const SOLANA_WEB3_PACKAGE = '@solana/web3.js'
 
 const uint8Array32 = fixedSizeUint8Array(32)
 
+/**
+ * De/Serializer for solana {@link PublicKey}s aka `publicKey`.
+ *
+ * @category beet/solana
+ */
 export const publicKey: Beet<PublicKey> = {
   write: function (buf: Buffer, offset: number, value: PublicKey): void {
     const arr = value.toBytes()
@@ -24,13 +29,29 @@ export const publicKey: Beet<PublicKey> = {
   description: 'PublicKey',
 }
 
+/**
+ * @category TypeDefinition
+ */
 export type KeysExports = keyof typeof import('./keys')
+/**
+ * @category TypeDefinition
+ */
 export type KeysTypeMapKey = 'publicKey'
+/**
+ * @category TypeDefinition
+ */
 export type KeysTypeMap = Record<
   KeysTypeMapKey,
   SupportedTypeDefinition & { beet: KeysExports }
 >
 
+/**
+ * Maps solana keys beet exports to metadata which describes in which package it
+ * is defined as well as which TypeScript type is used to represent the
+ * deserialized value in JavaScript.
+ *
+ * @category TypeDefinition
+ */
 export const keysTypeMap: KeysTypeMap = {
   publicKey: {
     beet: 'publicKey',
