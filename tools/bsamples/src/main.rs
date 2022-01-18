156510f12800597752dee1464d6f53970453407f
Thorsten Lorenz <thlorenz@gmx.de>
2022-01-17 19:18:12 -0600

    sample: including vecs

--- tools/bsamples/src/main.rs
+++ tools/bsamples/src/main.rs
@@ -4,10 +4,12 @@ use anyhow::Result;
 use borsh::BorshDeserialize;
 use options::produce_options;
 use simple::produce_simple;
+use vecs::produce_vecs;
 
 mod options;
 mod samples;
 mod simple;
+mod vecs;
 
 fn try_from_slice_unchecked<T: BorshDeserialize>(data: &[u8]) -> Result<T> {
     let mut data_mut = data;
@@ -26,6 +28,10 @@ fn main() -> Result<()> {
     let options_json = serde_json::to_string_pretty(&produce_options()?)?;
     let mut options_file = File::create(format!("{}/options.json", data_dir))?;
     options_file.write_all(options_json.as_bytes())?;
+
+    let vecs_json = serde_json::to_string_pretty(&produce_vecs()?)?;
+    let mut vecs_file = File::create(format!("{}/vecs.json", data_dir))?;
+    vecs_file.write_all(vecs_json.as_bytes())?;
 
     Ok(())
 }
