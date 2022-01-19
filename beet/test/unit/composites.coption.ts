35d5ea12f53ea81e74af30fdc5733157b65d1dc5
Thorsten Lorenz <thlorenz@gmx.de>
2022-01-19 13:14:52 -0600

    option: tests use coption instead of fixed size version

--- beet/test/unit/composites.coption.ts
+++ beet/test/unit/composites.coption.ts
@@ -1,22 +1,23 @@
 import {
   Beet,
-  fixedSizeOption,
   COption,
-  fixedSizeUtf8String,
-  FixedSizeBeet,
   u32,
   u8,
+  coption,
+  FixableBeet,
+  utf8String,
 } from '../../src/beet'
 import test from 'tape'
 
 function checkCases<T>(
   offsets: number[],
   cases: COption<T>[],
-  beet: FixedSizeBeet<COption<T>>,
+  fixableBeet: FixableBeet<COption<T>>,
   t: test.Test
 ) {
   for (const offset of offsets) {
     for (const x of cases) {
+      const beet = fixableBeet.toFixedFromValue(x)
       {
         // Larger buffer
         const buf = Buffer.alloc(offset + beet.byteSize + offset)
@@ -38,7 +39,7 @@ function checkCases<T>(
 test('composites: COption<u8>', (t) => {
   const cases = [1, 2, null, 0xff]
   const offsets = [0, 4]
-  const beet: Beet<COption<number>> = fixedSizeOption(u8)
+  const beet: Beet<COption<number>> = coption(u8)
 
   checkCases(offsets, cases, beet, t)
   t.end()
@@ -47,16 +48,16 @@ test('composites: COption<u8>', (t) => {
 test('composites: COption<u32>', (t) => {
   const cases = [1, null, 0xff, 0xffff, 0xffffffff]
   const offsets = [0, 4]
-  const beet: Beet<COption<number>> = fixedSizeOption(u32)
+  const beet: Beet<COption<number>> = coption(u32)
 
   checkCases(offsets, cases, beet, t)
   t.end()
 })
 
 test('composites: COption<string>', (t) => {
-  const cases = ['abc', 'xyz', null]
+  const cases = ['abc', 'abcxyz', null]
   const offsets = [0, 2]
-  const beet: Beet<COption<string>> = fixedSizeOption(fixedSizeUtf8String(3))
+  const beet: Beet<COption<string>> = coption(utf8String)
 
   checkCases(offsets, cases, beet, t)
   t.end()
