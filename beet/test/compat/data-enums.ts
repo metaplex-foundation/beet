4885b7c7555271b38e5d41b7a4fa5880aa45f2f7
Thorsten Lorenz <thlorenz@gmx.de>
2022-06-01 21:24:05 -0500

    test: collection info example

--- beet/test/compat/data-enums.ts
+++ beet/test/compat/data-enums.ts
@@ -5,7 +5,16 @@ import {
   checkFixableFromDataSerialization,
   checkFixableFromValueSerialization,
 } from '../utils'
-import { BeetArgsStruct, dataEnum, u32 } from '../../src/beet'
+import {
+  array,
+  BeetArgsStruct,
+  dataEnum,
+  FixableBeetArgsStruct,
+  u32,
+  u8,
+  uniformFixedSizeArray,
+  utf8String,
+} from '../../src/beet'
 
 function convertToKindData(value: any) {
   // The compat rust tool JSON stringifies the data enum however we represent this as a
@@ -26,10 +35,58 @@ test('compat data enums: simples', (t) => {
     },
   ])
 
-  for (const { value, data } of fixture.simples.slice(0, 1)) {
+  for (const { value, data } of fixture.simples) {
     const val = convertToKindData(value)
-    checkFixableFromDataSerialization(t, beet, val, data, 'simples from data')
-    checkFixableFromValueSerialization(t, beet, val, data, 'simples form value')
+    checkFixableFromDataSerialization(
+      t,
+      beet,
+      val,
+      data,
+      `${val.__kind} from data`
+    )
+    checkFixableFromValueSerialization(
+      t,
+      beet,
+      val,
+      data,
+      `${val.__kind} from data`
+    )
+  }
+  t.end()
+})
+
+test('compat data enums: CollectionInfo', (t) => {
+  const beet = dataEnum([
+    {
+      kind: 'V1',
+      dataBeet: new FixableBeetArgsStruct([
+        ['symbol', utf8String],
+        ['verified_creators', array(u8)],
+        ['whitelist_root', uniformFixedSizeArray(u8, 32)],
+      ]),
+    },
+    {
+      kind: 'V2',
+      dataBeet: new FixableBeetArgsStruct([['collection_mint', u8]]),
+    },
+  ])
+
+  for (const { value, data } of fixture.collections) {
+    const val = convertToKindData(value)
+    checkFixableFromDataSerialization(
+      t,
+      beet,
+      val,
+      data,
+      `${val.__kind} from data`
+    )
+    checkFixableFromValueSerialization(
+      t,
+      beet,
+      val,
+      data,
+      `${val.__kind} from value`
+    )
   }
   t.end()
 })
