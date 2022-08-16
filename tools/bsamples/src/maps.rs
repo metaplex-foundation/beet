36357877de5227d6e2f5ec337835c477b2516a34
Thorsten Lorenz <thlorenz@gmx.de>
2022-08-16 11:05:54 -0500

    chore: add sample of hashmap with fixable key/val items

--- tools/bsamples/src/maps.rs
+++ tools/bsamples/src/maps.rs
@@ -10,6 +10,7 @@ use crate::samples::{produce_samples, Sample};
 pub struct Maps {
     hash_map_u8_u8s: Vec<Sample<HashMap<u8, u8>>>,
     btree_map_u8_u8s: Vec<Sample<BTreeMap<u8, u8>>>,
+    hash_map_string_vec_i8s: Vec<Sample<HashMap<String, Vec<i8>>>>,
 }
 
 pub fn produce_maps() -> Result<Maps> {
@@ -17,18 +18,38 @@ pub fn produce_maps() -> Result<Maps> {
     let p2 = [(11, 111), (22, 122), (33, 133)];
     let p3 = [(111, 110), (222, 220), (255, 250)];
 
-    let hm1 = HashMap::from(p1);
-    let hm2 = HashMap::from(p2);
-    let hm3 = HashMap::from(p3);
-    let hash_map_u8_u8s = produce_samples(vec![hm1, hm2, hm3])?;
+    let hash_map_u8_u8s = {
+        let hm1 = HashMap::from(p1);
+        let hm2 = HashMap::from(p2);
+        let hm3 = HashMap::from(p3);
+        produce_samples(vec![hm1, hm2, hm3])?
+    };
 
-    let btm1 = BTreeMap::from(p1);
-    let btm2 = BTreeMap::from(p2);
-    let btm3 = BTreeMap::from(p3);
-    let btree_map_u8_u8s = produce_samples(vec![btm1, btm2, btm3])?;
+    let btree_map_u8_u8s = {
+        let btm1 = BTreeMap::from(p1);
+        let btm2 = BTreeMap::from(p2);
+        let btm3 = BTreeMap::from(p3);
+        produce_samples(vec![btm1, btm2, btm3])?
+    };
+
+    let hash_map_string_vec_i8s = {
+        let hm1 = HashMap::from([
+            ("Uno".to_string(), vec![-3, -2, -1]),
+            ("Dos".to_string(), vec![-1, 0, 1, 2, 3, 4]),
+            ("Tres".to_string(), vec![-3, -2, -1, 0, 1]),
+        ]);
+        let hm2 = HashMap::from([
+            ("Eins".to_string(), vec![3, 2, 1]),
+            ("Zwei".to_string(), vec![1, 0, -1, -2, -3, -4]),
+            ("Drei".to_string(), vec![-3, -2, -1, 0, 1]),
+            ("Vier".to_string(), vec![]),
+        ]);
+        produce_samples(vec![hm1, hm2])?
+    };
 
     Ok(Maps {
         hash_map_u8_u8s,
         btree_map_u8_u8s,
+        hash_map_string_vec_i8s,
     })
 }
